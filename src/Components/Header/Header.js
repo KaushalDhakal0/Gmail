c84933489ecf569fe11e492e98a2ca00a480a3a5
KaushalDhakal <kaushaldhakal90@gmail.com>
2022-01-25 21:14:07 -0800

    working on header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src="https://cdn.vox-cdn.com/thumbor/Tbqi3ZF9Qz0fTJIUvkgQe3FdN0k=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg" />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_SENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+//init firebase app
+initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+const auth = getAuth();
+
+export { db, auth };
