a13ce7d167d6dfe87b7ab58674ba7d549610da15
KaushalDhakal <kaushaldhakal90@gmail.com>
2022-02-07 16:04:56 -0800

    model done

--- src/Components/Mail/SendMail.js
+++ src/Components/Mail/SendMail.js
@@ -7,6 +7,9 @@ const SendMail = () => {
   const onSubmit = (data) => {
     console.log(data);
   };
+  const handleClose = () => {
+    console.log("close button clicked.");
+  };
   const {
     register,
     handleSubmit,
@@ -17,27 +20,36 @@ const SendMail = () => {
     <div className="sendMail">
       <div className="sendMail__header">
         <h3>New Message</h3>
-        <Close className="sendMail__close" />
+        <Close className="sendMail__close" onClick={handleClose} />
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          name="To"
           type="text"
           placeholder="to"
           {...register("to", { required: true, pattern: /^\S+@\S+$/i })}
         />
-        {errors.to && <p>{"Enter valid email"}</p>}
+        {errors.to && <p className="error__message">{"Enter valid email"}</p>}
         <input
+          name="subject"
           type="text"
           placeholder="subject"
-          {...register("Subject", { required: true })}
+          {...register("subject", { required: true })}
         />
+        {errors.subject && (
+          <p className="error__message">{"Subject is invalid"}</p>
+        )}
 
         <input
+          name="message"
           type="text"
           placeholder="message..."
           className="sendMail__message"
           {...register("message", { required: true })}
         />
+        {errors.message && (
+          <p className="error__message">{"message cannot be empty"}</p>
+        )}
         <div className="sendMail__options">
           <Button
             className="sendMail__send"
